2436304b849510a7ba44f5857f2ee19e83d0033c
AkhilAjith2 <akhilajith2003@gmail.com>
2025-01-14 23:23:42 +0100

    fixed history page

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,9 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UploadViewSet, ProfileView, UploadHistoryView, RegisterUserView, CustomObtainAuthToken, LoginView, test_view, get_csrf_token
+from .views import UploadViewSet, ProfileView, UploadHistoryView, RegisterUserView, CustomObtainAuthToken, LoginView, test_view, get_csrf_token, FetchImage, FetchMIDI
 from rest_framework.authtoken.views import obtain_auth_token
 from django.views.decorators.csrf import csrf_exempt
-
 
 router = DefaultRouter()
 router.register(r'upload', UploadViewSet, basename='upload')
@@ -14,10 +13,10 @@ urlpatterns = [
     path('', include(router.urls)),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('history/', UploadHistoryView.as_view(), name='upload-history'),
+    path('history/<int:upload_id>/', FetchImage.as_view(), name='upload-history-detail'),
+    path('history/<int:upload_id>/fetch-midi/', FetchMIDI.as_view(), name='fetch_midi'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('auth/login/', csrf_exempt(LoginView.as_view()), name='custom_login'),
     path('test/', test_view),
     path('csrf-token/', get_csrf_token, name='csrf_token'),
-
 ]
-
--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 # api/views.py
 import os
 
-from django.http import FileResponse
 from omr_model_files.model_extraction_and_prediction import extract_staves, predict
 from django.conf import settings
+from django.shortcuts import render
 from omr_model_files.semantic_to_midi import semantic_to_midi, remove_extra_metadata
-from omr_model_files.midi_to_mei import convert_midi_to_mei_with_musescore, merge_mei_files, merge_mei_batches
+from omr_model_files.midi_to_mei import convert_midi_to_mei_with_musescore, merge_mei_files
 from omr_model_files.midi_left_and_right_combiner import merge_midi_files, merge_multiple_midi_files
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
@@ -20,7 +20,9 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
-
+from django.shortcuts import get_object_or_404
+from django.http import FileResponse
+from django.core.cache import cache
 
 # ViewSet for managing uploads
 class UploadViewSet(viewsets.ModelViewSet):
@@ -144,93 +146,12 @@ class UploadViewSet(viewsets.ModelViewSet):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    # @action(detail=True, methods=["post"])
-    # def convert(self, request, pk=None):
-    #     upload = self.get_object()
-    #     if upload.status != "pending":
-    #         return Response({"error": "File is already processed or in progress."}, status=status.HTTP_400_BAD_REQUEST)
-    #
-    #     tempo = float(request.data.get("tempo"))
-    #     time_signature = request.data.get("timeSignature")
-    #
-    #     # Update status to 'processing'
-    #     upload.status = "processing"
-    #     upload.save()
-    #
-    #     # Extract staves
-    #     try:
-    #         file_path = upload.file.path
-    #         output_dir = os.path.join(settings.MEDIA_ROOT, "extracted_staves")
-    #         final_midi_dir = os.path.join(settings.MEDIA_ROOT, "converted")
-    #         final_mei_dir = os.path.join(settings.MEDIA_ROOT, "mei")
-    #         os.makedirs(final_midi_dir, exist_ok=True)
-    #         staves = extract_staves(file_path, output_dir)
-    #         print(staves)
-    #         # Run predictions on extracted staves
-    #         model_path = "omr_model_files/Camera-PrIMuS_hybrid_semantic_v1-10-10.meta"
-    #         voc_path = "omr_model_files/vocabulary_semantic.txt"
-    #         midi_files = []
-    #         left_predictions = []
-    #         right_predictions = []
-    #         for i in range(len(staves)):
-    #             left, right = staves[i]
-    #             left_predictions.extend(predict(left, model_path, voc_path))
-    #             left_predictions.append("barline")
-    #             right_predictions.extend(predict(right, model_path, voc_path))
-    #             right_predictions.append("barline")
-    #
-    #         print(f"Left prediction:{left_predictions}\n Right prediction:{right_predictions}\n")
-    #
-    #         cleaned_left_predictions = remove_extra_metadata(left_predictions)
-    #         cleaned_right_predictions = remove_extra_metadata(right_predictions)
-    #
-    #         print(f"Left prediction:{cleaned_left_predictions}\n Right prediction:{cleaned_right_predictions}\n")
-    #
-    #         left_midi_file = f"{final_midi_dir}/left_{upload.id}.mid"
-    #         right_midi_file = f"{final_midi_dir}/right_{upload.id}.mid"
-    #         semantic_to_midi(cleaned_left_predictions, left_midi_file, tempo, time_signature)
-    #         semantic_to_midi(cleaned_right_predictions, right_midi_file, tempo, time_signature)
-    #
-    #         left_mei_file = f"{final_mei_dir}/left_{upload.id}.mei"
-    #         right_mei_file = f"{final_mei_dir}/right_{upload.id}.mei"
-    #         convert_midi_to_mei_with_musescore(left_midi_file,left_mei_file)
-    #         convert_midi_to_mei_with_musescore(right_midi_file, right_mei_file)
-    #
-    #         merged_mei_file = f"{final_mei_dir}/merged_{upload.id}.mei"
-    #         merge_mei_files(left_mei_file, right_mei_file, time_signature, merged_mei_file)
-    #
-    #         # Merge all MIDI pairs into a single file
-    #         final_midi_file = os.path.join(final_midi_dir, f"merged_{upload.id}.mid")
-    #         merge_midi_files(left_midi_file, right_midi_file, final_midi_file)
-    #
-    #         # Update the Upload model instance
-    #         upload.converted_file.name = os.path.relpath(final_midi_file, settings.MEDIA_ROOT)
-    #         upload.converted_mei_file.name = os.path.relpath(merged_mei_file, settings.MEDIA_ROOT)
-    #         print(upload.converted_file.name)
-    #         print(upload.converted_mei_file.name)
-    #         upload.status = "completed"
-    #         upload.save()
-    #
-    #         return Response({"status": "Conversion completed"}, status=status.HTTP_200_OK)
-    #     except Exception as e:
-    #         upload.status = "pending"  # Reset status on failure
-    #         upload.save()
-    #         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
     @action(detail=True, methods=["get"])
     def status(self, request, pk=None):
         # Return the current status of the file, Check the status of a file conversion.
 
         upload = self.get_object()
         return Response({"status": upload.status}, status=status.HTTP_200_OK)
-
-    # @action(detail=True, methods=["get"])
-    # def download(self, request, pk=None):
-    #     # Return the converted file for download, Download a converted MIDI file.
-    #     upload = self.get_object()
-    #     if upload.converted_file:
-    #         return Response({"file_url": upload.converted_file.url}, status=status.HTTP_200_OK)
-    #     return Response({"error": "File not converted yet."}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=["get"])
     def mei(self, request, pk=None):
@@ -265,7 +186,6 @@ class UploadViewSet(viewsets.ModelViewSet):
         # If the file doesn't exist, return an error response
         return Response({"error": "Converted MIDI file not found."}, status=status.HTTP_404_NOT_FOUND)
 
-
 # ViewSet for managing profiles
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
@@ -286,7 +206,6 @@ class ProfileView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
-
 class UploadHistoryView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -297,6 +216,25 @@ class UploadHistoryView(APIView):
         serializer = UploadHistorySerializer(uploads, many=True)
         return Response(serializer.data)
 
+class FetchImage(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, upload_id):
+        upload = get_object_or_404(Upload, id=upload_id, user=request.user)
+        return FileResponse(upload.file.open(), as_attachment=True, filename=upload.file.name)
+
+class FetchMIDI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, upload_id):
+        upload = get_object_or_404(Upload, id=upload_id, user=request.user)
+
+        # Ensure the converted MIDI file exists
+        if not upload.converted_file:
+            return Response({"error": "Converted MIDI file not found."}, status=404)
+
+        # Return the converted MIDI file as an attachment
+        return FileResponse(upload.converted_file.open(), as_attachment=True, filename=upload.file.name)
 
 class RegisterUserView(APIView):
     def post(self, request):
@@ -313,7 +251,6 @@ class RegisterUserView(APIView):
         user = User.objects.create_user(username=username, password=password, email=email)
         return Response({'message': 'User created successfully.'}, status=status.HTTP_201_CREATED)
 
-
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         # Print the request data for debugging
@@ -321,7 +258,6 @@ class CustomObtainAuthToken(ObtainAuthToken):
         response = super().post(request, *args, **kwargs)
         print("Response data:", response.data)
         return response
-
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
